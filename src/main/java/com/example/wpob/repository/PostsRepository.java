e9e63ed5e144831ff44eef44d22ebb3f75929c1a
hcgo97 <hcgo_o@naver.com>
2023-08-12 15:15:46 +0900

    feat_02: User, Post JPA Repository 추가

---
+++ src/main/java/com/example/wpob/repository/PostsRepository.java
@@ -0,0 +1,10 @@
+package com.example.wpob.repository;
+
+import com.example.wpob.entity.Posts;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostsRepository extends JpaRepository<Posts, Long> {
+
+}
