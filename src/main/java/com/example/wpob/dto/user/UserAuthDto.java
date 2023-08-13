6264d61ad08ca2487e4f63833eda4445edd94dad
hcgo97 <hcgo_o@naver.com>
2023-08-14 00:22:46 +0900

    refactor: BaseController 에서 유저 인증 정보를 가져오기 위하여 UserAuthDto 수정

--- src/main/java/com/example/wpob/dto/user/UserAuthDto.java
+++ src/main/java/com/example/wpob/dto/user/UserAuthDto.java
@@ -13,20 +13,19 @@ import java.util.Collection;
 public class UserAuthDto implements UserDetails {
 
     private String token;
-    private String userId; // users PK
+    private Long userId; // users PK
     private String email; // login id
 
     @Builder
     public UserAuthDto(String token, String userId, String email) {
         this.token = token;
-        this.userId = userId;
+        this.userId = Long.valueOf(userId);
         this.email = email;
     }
 
     @Override
     public String getUsername() {
-        // SecurityContextHolder.getContext().getAuthentication().getPrincipal()에서 사용
-        return this.userId;
+        return this.email;
     }
 
     @Override
